304f135bf3205b3e6458e00db6ec7858440e90c0
AlexanderDmitriev <alex9110077@gmail.com>
2024-09-16 00:59:45 +0200

    start

---
+++ src/app/common-ui/profie-card/profie-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profie-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './profie-card.component.html',
+  styleUrl: './profie-card.component.scss'
+})
+export class ProfieCardComponent {
+
+}

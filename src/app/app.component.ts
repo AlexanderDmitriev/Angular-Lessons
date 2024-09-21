b283df690df0b7b628dbe9466cab2168024699f0
AlexanderDmitriev <alex9110077@gmail.com>
2024-09-21 18:11:19 +0200

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfieCardComponent } from "./common-ui/profie-card/profie-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProfieCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
